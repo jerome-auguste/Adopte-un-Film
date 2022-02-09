1e43c15b121ee25bc8580e5ab823a116ef79d80d
Nathan Bruckmann <nathan.bruckmann@student-cs.fr>
2022-02-09 17:42:28 +0100

    clean search by director

--- requests.py
+++ requests.py
@@ -1,48 +1,33 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 
-sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+def get_sparql():
+    return SPARQLWrapper("http://dbpedia.org/sparql")
 
-# Example
-# sparql.setQuery("""
-#     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-#     SELECT ?label
-#     WHERE { <http://dbpedia.org/resource/Asturias> rdfs:label ?label }
-# """)
+def get_prefix():
+    return """
+    prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    prefix dbo: <http://dbpedia.org/ontology/>
+    prefix dbo: <http://dbpedia.org/property/>
+    """
 
-# Test 1
-# sparql.setQuery("""
-# prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-# prefix db: <http://dbpedia.org/ontology/>
-# SELECT DISTINCT ?film ?director 
-# WHERE {
-# ?film a db:Film .
-# ?director a db:Person .
-# ?director rdfs:director ?film .
-# } LIMIT 50
-# """)
+def get_film_from(director, split=True):
+    if split:
+        part_list = [f"'{part}'" for part in director.split()]
+        director = "and".join(part_list)
+    query = get_prefix() + """
+    SELECT DISTINCT ?filmName ?dirName
+    WHERE {
+    ?film dbo:director ?director .
+    ?director dbp:name ?dirName .
+    ?film dbp:name ?filmName .
+    ?dirName bif:contains "'nolan' and 'christopher'" .
+    } LIMIT 50
+    """
+    sp = get_sparql()
+    sp.setQuery(query)
+    sp.setReturnFormat(JSON)
+    results = sp.query().convert()
+    return [r["filmName"]["value"] for r in results["results"]["bindings"]]
 
-sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+print(get_film_from("christopher nolan"))
 
-sparql.setQuery("""
-PREFIX bd: <http://www.bigdata.com/rdf#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX schema: <http://schema.org/>
-PREFIX wd: <http://www.wikidata.org/entity/>
-PREFIX wdt: <http://www.wikidata.org/prop/direct/>
-
-SELECT ?director ?director_label ?film ?film_label
-WHERE {
-?director wdt:P106 wd:Q2526255 .                 # has "film director" as occupation
-?film wdt:P57 ?director .
-SERVICE wikibase:label {
-  bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en".  # Get label if it exists
-?director rdfs:label ?director_label .
-?film rdfs:label ?film_label }
-} LIMIT 100
-""")
-
-sparql.setReturnFormat(JSON)
-results = sparql.query().convert()
-print(results)
-for result in results["results"]["bindings"]:
-    print(result["director_label"]["value"], result["film_label"]["value"])
