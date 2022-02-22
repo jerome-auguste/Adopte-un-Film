a7679b418a3a0143bc57884ee36fc1148c31f3ea
Nathan Bruckmann <nathan.bruckmann@student-cs.fr>
2022-02-22 15:48:49 +0100

    by genre

--- requests.py
+++ requests.py
@@ -29,23 +29,27 @@ def search(variable, target):
     }}
     """
 
-def get_film(title=None, director=None, actor=None):
+def get_film(title=None, director=None, actor=None, genre=None):
     query = f"""
     {get_prefix()}    
     SELECT ?filmLabel ?directorLabel 
            (GROUP_CONCAT(DISTINCT ?actorLabel; separator=", ") as ?actorsLabel)
+           (GROUP_CONCAT(DISTINCT ?genreLabel; separator=", ") as ?genresLabel)
     WHERE {{
         ?film wdt:P57 ?director ;
-              wdt:P161 ?actor .
+              wdt:P161 ?actor ;
+              wdt:P136 ?genre .
         SERVICE wikibase:label {{
             bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en".
             ?film rdfs:label ?filmLabel .
             ?director rdfs:label ?directorLabel .
             ?actor rdfs:label ?actorLabel .
+            ?genre rdfs:label ?genreLabel .
         }}
         {search('?film', title)}
         {search('?director', director)} 
-        {search('?actor', actor)} 
+        {search('?actor', actor)}
+        {search('?genre', genre)} 
     }}
     GROUP BY ?filmLabel ?directorLabel
     LIMIT 100
@@ -56,5 +60,5 @@ def get_film(title=None, director=None, actor=None):
     sp.setReturnFormat(JSON)
     return sp.query().convert()['results']['bindings']
 
-res = get_film(title='dark knight')
+res = get_film(director='nolan')
 pprint(res)
