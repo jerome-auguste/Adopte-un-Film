a70424c6359a0477cee9b2bbbe9bc2dc7c41c3d7
Ruben Partouche <rpartouche@icebergdatalab.com>
2022-02-26 13:42:25 +0100

    fix from rebase

--- sparqlRequests.py
+++ sparqlRequests.py
@@ -79,7 +79,39 @@ def get_film(title=None, director=None, actor=None, genre=None, score=0):
     GROUP BY ?film ?filmLabel ?directorLabel ?score
     LIMIT 100
     """
-    # print(query)
+    print(query)
+    sp = get_sparql()
+    sp.setQuery(query)
+    sp.setReturnFormat(JSON)
+    return format(sp.query().convert()['results']['bindings'])
+
+
+def recommendation(film, limit=20):
+    query = f"""
+    {get_prefix()}
+    SELECT ?film ?filmLabel ?topicLabel ?score
+    WHERE {{
+    {{
+        SELECT ?topic
+        WHERE {{ {film} wdt:P921 ?topic . }}
+    }}
+    ?film wdt:P31 wd:Q11424 ;
+          wdt:P921 ?topic ;
+          wdt:P444 ?brutScore .
+    SERVICE wikibase:label {{
+        bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en" .
+        ?film rdfs:label ?filmLabel .
+        ?topic rdfs:label ?topicLabel .
+    }}
+    FILTER regex(?brutScore, "^[0-9]+%$")
+    BIND(xsd:integer(REPLACE(?brutScore, "%$", "")) AS ?score)
+    FILTER (?score != 100)
+    FILTER(?film != {film})
+    }}
+    ORDER BY DESC(?score)
+    LIMIT {limit}
+    """
+    print(query)
     sp = get_sparql()
     sp.setQuery(query)
     sp.setReturnFormat(JSON)
